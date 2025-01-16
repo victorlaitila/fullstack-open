36735735ee6b979e3ef4001377d77aa3c717560e
victorlaitila <viclait@gmail.com>
2025-01-16 16:23:26 +0200

    Countries with weather API

--- part2/countries/components/CountryList.jsx
+++ part2/countries/components/CountryList.jsx
@@ -1,51 +1,57 @@
-const CountryList = ({countries}) => {
+const CountryList = ({countries, selectedCountry, weatherData, weatherIcon, showCountryData}) => {
+  if (selectedCountry) {
+    return <CountryData country={selectedCountry} weatherData={weatherData} weatherIcon={weatherIcon} />
+  }
+  if (countries.length === 1) {
+    return <CountryData country={countries[0]} weatherData={weatherData} weatherIcon={weatherIcon} />
+  }
   if (countries.length > 10) {
     return <div>Too many matches, specify another filter</div>
   }
   if (countries.length > 1 && countries.length <= 10) {
-    return (
-      countries.map((c) => <Country key={c.name.common} name={c.name.common} />)
-    )
-  }
-  if (countries.length === 1) {
-    return (
-      <CountryData country={countries[0]} />
-    )
+    return countries.map((c) => <Country key={c.name.common} name={c.name.common} showCountryData={() => showCountryData(c.name.common)} />)
   }
 }
 
-const Country = ({name}) => {
-  return (
-    <div>
-      {name} <button onClick={() => console.log('show data for country')}>show</button>
-    </div>
-  )
-}
+const Country = ({name, showCountryData}) => <div>{name} <button onClick={showCountryData}>show</button></div>
 
-const CountryData = ({country}) => {
+const CountryData = ({country, weatherData, weatherIcon}) => {
   return (
     <div>
       <h1>{country.name.common}</h1>
-      <p>Capital: {country.capital}</p>
+      <div>Capital: {country.capital}</div>
+      <div>Area: {country.area}</div>
       <h3>Languages:</h3>
       <LanguageList languages={country.languages} />
       <img src={country.flags.svg} width={250} height={250} alt="Image of flag" />
+      <CapitalWeather weatherData={weatherData} weatherIcon={weatherIcon} />
     </div>
   )
 }
 
 const LanguageList = ({languages}) => {
   return (
-    <div>
+    <ul>
       {Object.entries(languages).map(([code, name]) => (
-        <div key={code}>{name}</div>
+        <li key={code}>{name}</li>
       ))}
       <br></br>
-    </div>
+    </ul>
   );
 };
 
-
+const CapitalWeather = ({weatherData, weatherIcon}) => {
+  if (weatherData) {
+    return (
+      <div>
+        <h3>Weather in {weatherData.name}</h3>
+        <div>Temperature {weatherData.main.temp} Celcius</div>
+        <img src={weatherIcon} alt="Weather icon"></img>
+        <div>Wind {weatherData.wind.speed} m/s</div>
+      </div>
+    )
+  }
+}
 
 export default CountryList
 
--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,11 +1,16 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react"
 import axios from "axios";
-import CountryList from '../components/CountryList';
+import CountryList from "../components/CountryList";
 
 const App = () => {
-  const [searchQuery, setSearchQuery] = useState('')
+  const [searchQuery, setSearchQuery] = useState("")
   const [countries, setCountries] = useState([])
   const [filteredCountries, setFilteredCountries] = useState([])
+  const [selectedCountry, setSelectedCountry] = useState(null)
+  const [weatherData, setWeatherData] = useState(null)
+  const [weatherIcon, setWeatherIcon] = useState(null)
+
+  const WEATHER_API_KEY = import.meta.env.VITE_WEATHER_KEY
 
   useEffect(() => {
     getAllCountries()
@@ -13,24 +18,54 @@ const App = () => {
 
   const getAllCountries = async () => {
     try {
-      const response = await axios.get('https://studies.cs.helsinki.fi/restcountries/api/all')
+      const response = await axios.get("https://studies.cs.helsinki.fi/restcountries/api/all")
       setCountries(response.data)
     } catch (e) {
       console.error("Error fetching countries", e)
     }
   }
 
-  const handleSearchQuery = (event) => {
+  const getCapitalWeather = async (capital) => {
+    try {
+      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${WEATHER_API_KEY}&units=metric`)
+      setWeatherData(response.data)
+      const icon = response.data.weather[0].icon
+      setWeatherIcon(`https://openweathermap.org/img/wn/${icon}@2x.png`)
+    } catch (e) {
+      console.error("Error fetching weather data", e)
+    }
+  }
+
+  const handleSearchQuery = async (event) => {
+    setWeatherData(null)
+    setWeatherIcon(null)
+    setSelectedCountry(null)
     const newQuery = event.target.value
     setSearchQuery(newQuery)
-    const updatedCountryList = countries.filter((country) => country.name.common.toLowerCase().includes(newQuery.toLowerCase())) 
+    const updatedCountryList = countries.filter((c) => c.name.common.toLowerCase().includes(newQuery.toLowerCase())) 
     setFilteredCountries(updatedCountryList)
+    // Showing detailed data for country
+    if (updatedCountryList.length === 1) {
+      getCapitalWeather(updatedCountryList[0].capital)
+    }
+  }
+
+  const showCountryData = (countryName) => {
+    const newSelectedCountry = countries.find((c) => c.name.common === countryName)
+    setSelectedCountry(newSelectedCountry)
+    getCapitalWeather(newSelectedCountry.capital)
   }
 
   return (
     <div>
       Find countries <input value={searchQuery} onChange={handleSearchQuery}></input>
-      <CountryList countries={filteredCountries} />
+      <CountryList 
+        countries={filteredCountries} 
+        selectedCountry={selectedCountry} 
+        weatherData={weatherData}
+        weatherIcon={weatherIcon}
+        showCountryData={showCountryData} 
+      />
     </div>
   )
 }
